40d19e358db1a96e10ee3ac3711549034d636e39
Ruan Nunes <ruan.nunes@el.com.br>
2018-05-16 23:48:39 -0300

    Atualização

---
+++ WebProject1/variaveis.py
@@ -0,0 +1,17 @@
+class variaveis(object):
+    #variaveis nunca podem ser iniciadas com numero e nem conter espaços
+    #nomes explicativos para variaveis
+    # palavras reservadas para o python e não podem ser definidas como variaveis
+        #and , as, asset, break, class, continue, def, del ,elif, if, else, except, exec, finality, for, from , global, import, in ,is , lambda, noniocal, not, or, pass, reise, return, try, while, with, yield, True, False, None
+    minha_idade = 19
+    print('Eu tenho ', minha_idade, 'anos')
+
+
+    num = 1
+    num2 = 2
+    print('soma dos numero ', num ,'+', num2,'é igual ', num + num2)
+    pass
+
+
+
+
