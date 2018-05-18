5b53ff1dad960157b3fedcb4d15c6745083061c8
Ruan Nunes <ruan.nunes@el.com.br>
2018-05-17 23:37:08 -0300

    Novas classes

---
+++ WebProject1/numeros.py
@@ -0,0 +1,14 @@
+class numeros(object):
+    # existem varios tipos de numeros (int = inteiros, flout = decimais, entre outros)
+    num1 = 2
+    num2 = 5.5
+    print(type(num1))
+    print(type(num2))
+    num3 = num1 + num2
+    print(type(num3))
+
+    pass
+
+
+
+
---
+++ WebProject1/primeiroPrograma.py
@@ -0,0 +1,11 @@
+class primeiroPrograma(object):
+    print('Meu Primeiro Programa')
+    num1 = int(input('Digite Um Numero:'))
+    num2 = int(input('Digite o Segundo Numero:'))
+
+    print('A soma dos numeros é:', num1 + num2)
+    pass
+
+
+
+
